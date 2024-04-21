bed7d944b153ce0e9d5191ceaf78ffd1d708972b
YanaBoyko1 <150178023+YanaBoyko1@users.noreply.github.com>
2024-04-21 23:07:26 +0300

    Create test_prefix_trie.py and add unittesr_code about "prefix_trie"

---
+++ test/test_prefix_trie.py
@@ -0,0 +1,23 @@
+from unittest import TestCase, main
+from prefix_trie import Trie
+
+
+class TestTrie(TestCase):
+    def setUp(self):
+        self.trie = Trie()
+
+    def test_insert_and_search(self):
+        self.trie.insert("apple")
+        self.assertTrue(self.trie.search("apple"))
+        self.assertFalse(self.trie.search("app"))
+        self.assertFalse(self.trie.search("banana"))
+
+    def test_start_with(self):
+        self.trie.insert("apple")
+        self.trie.insert("app")
+        self.assertTrue(self.trie.start_with("app"))
+        self.assertFalse(self.trie.start_with("ban"))
+
+
+if __name__ == '__main__':
+    main()
