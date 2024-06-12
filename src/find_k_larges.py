42f29f662a0e72ce39ed31ccdf3d196cd6a1b10b
YanaBoyko1 <150178023+YanaBoyko1@users.noreply.github.com>
2024-06-12 21:30:20 +0300

    Removed output examples

--- src/find_k_larges.py
+++ src/find_k_larges.py
@@ -15,13 +15,3 @@ def find_k_largest(num, k):
     k_largest = sorted_nums[-k]
     position = num[::-1].index(k_largest)
     return k_largest, position
-
-
-num = [15, 7, 22, 9, 36, 2, 42, 18]
-k = 3
-result = find_k_largest(num, k)
-k_largest, position = result
-print(f"Знайдений {k}-й найбільший елемент: {k_largest}")
-print(f"Позиція {k}-го найбільшого елемента в масиві: {position}")
-print(sorted_numbers(num))
-print(result)
